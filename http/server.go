d17edc79e3ce8f057f06ec3dc548cbc67c3e9c9a
Recep <recep4sirin@gmail.com>
2021-02-13 23:04:57 +0300

    added an example of serving static files

---
+++ http/server.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+    "net/http"
+)
+
+func main() {
+    fs := http.FileServer(http.Dir("static/"))
+    http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+    http.ListenAndServe(":8012", nil)
+}
