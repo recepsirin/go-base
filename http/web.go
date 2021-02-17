719ca13eedc8a13602d2e7899c4010e6efb8444e
Recep <recep4sirin@gmail.com>
2021-02-17 22:44:20 +0300

    added routing

--- http/web.go
+++ http/web.go
@@ -3,14 +3,21 @@ package main
 import (
     "log"
     "net/http"
+    "time"
 )
 
 func index(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("<h1>Cool header</h1>"))
+    w.Write([]byte("<h1>Cool header " + time.Now().Format("01-01-2006 15:04:05 Mon") + "</h1>"))
+}
+
+func pageHandler(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "http://google.com", 301)
 }
 
 func main() {
     mux := http.NewServeMux()
     mux.HandleFunc("/", index)
+    mux.HandleFunc("/route", pageHandler)
+
     log.Fatal(http.ListenAndServe(":5000", mux))
 }
